ce7f34f3ee49f088252fcb0901bd73d2685d06e1
Alexandr <kuvaldin.ai95@gmail.com>
2020-10-12 19:45:28 +0700

    Выполнил задание к 3-у уроку

--- script/script.js
+++ script/script.js
@@ -1,19 +1,43 @@
 'use strict';
-//1) Создать переменную num со значением 266219 (тип данных число)
- const num = 266219;
 
-//2) Вывести в консоль произведение (умножение) цифр этого числа
- const arr = num.toString().split('');
+/*Переменная lang может принимать 2 значения: 'ru' 'en'.
+Написать условия при котором в зависимости от значения 
+lang будут выводится дни недели на русском или английском языке.*/
+//через if, 
+let lang = prompt ('Выберите язык (ru/en)');
 
- 
- const result = (accumulator, currentValue) =>accumulator * currentValue ;
-    console.log(arr.reduce(result));
-/*3) Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)*/
-const sum = Number(arr.reduce(result));
-const degree = sum ** 3;
-console.log(degree);
+ if (lang === 'ru') {
+     console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+ } else if (lang === 'en') {
+     console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
+ } else {
+     console.log('Введено неправильное значение');
+ }
+ //через switch-case 
+ switch (lang) {
+     case 'ru':
+         console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+         break;
+    case 'en':
+        console.log('Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday');
+        break;
+ }
 
-//4) Вывести на экран первые 2 цифры полученного числа
-const sub = String(degree);
-console.log(sub.substring(0,2));
+ //через многомерный массив без ифов и switch.
+ let days = [];
+  days ['ru'] = ['Понедельник', 'Вторник', 'Среда', 
+            'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+  days ['en'] = ['Sunday', 'Monday', 'Tuesday', 
+            'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+let langUser = lang;
+console.log(days[langUser]);
+
+//№3
+let namePerson = prompt ('Введите Ваше имя');
+
+    let Person = 
+    (namePerson === 'Артем') ? 'Директор' : 
+    (namePerson === 'Максим') ? 'Преподаватель' :
+    'Студент';
+    console.log(Person);
